0098a66d00375b2d2bcbc859759489196289dce7
Alexandre <alexandre.h.falardeau@gmail.com>
2018-12-01 16:25:14 -0500

    home page, navbar and map page need to be styled

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,32 @@
+//@ts-check
 import React, { Component } from 'react';
-import './App.css';
+import {BrowserRouter, Route} from "react-router-dom";
 import Map from './components/map/PolygonMap';
+import Home from "./components/Home.js";
+import Navbar from "./components/Navbar.js";
+import ErrorPage from "./components/Error.js";
+
+import './App.css';
+
+// import ErrorPage from "./Error.jsx";
 
 class App extends Component {
   render() {
     return (
-      <Map/>
+      <div>
+        <BrowserRouter>
+          <div>
+              <Navbar/>
+              <Route path="/" component={Home} exact />
+              <Route path="/map" component={Map} exact/>
+              <Route  component={ErrorPage}/>
+          </div>
+        </BrowserRouter>
+      </div>
     );
   }
 }
+
 
 
 export default App;
---
+++ frontend/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function HomePage(props) {
+    // You can compute or insert data for the homepage here if you want
+    return(
+        <div>
+            {/* Put the HTML here, 
+            if you want to use classes, use the clasnName attribute instead of the class sticker */}
+        </div>
+    )
+}
