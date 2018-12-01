3f254593237ff2103e772aadecbed3257ba98006
Valerian Clerc <valerian.clerc@mail.mcgill.ca>
2018-12-01 11:42:01 -0500

    added setup, GCP auth, and made GCP vision API work

---
+++ backend/setup.bash
@@ -0,0 +1,7 @@
+pip3 install --user asyncio
+pip3 install --user numpy
+pip3 install --user opencv-python
+pip3 install --user google-cloud-vision
+
+
+export GOOGLE_APPLICATION_CREDENTIALS="./yhackGCP.json"
