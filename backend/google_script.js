3ff4c7cab0c2b5e388b2db49ace3854e7eefc97b
Alexandre <alexandre.h.falardeau@gmail.com>
2018-12-02 06:10:45 -0500

    changes to the google call api

--- backend/google_script.js
+++ backend/google_script.js
@@ -1,4 +1,5 @@
 const index = require('./index');
+const Promise = require('bluebird')
 
 //@ts-check
 var fs = require('fs'),
@@ -11,7 +12,6 @@ var download = function(uri, filename, callback){
     request.head(uri, function(err, res, body){
       console.log('content-type:', res.headers['content-type']);
       console.log('content-length:', res.headers['content-length']);
-	   
       request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
     });
   };
@@ -36,14 +36,22 @@ function downloadLocationPicture(locationName, locationbias, filename) {
                         reject(err)
                     } else {
                         let uri = "https://" + response.req.socket._host + response.req.path;
-                        download(uri, filename, function(){
-                            resolve("good");
+                        download(uri, filename, function(err){
+                            if(err){
+                                reject(err);
+                            } else {
+                                console.log("suppposed to resolve")
+                                resolve({
+                                    name:filename,
+                                    location:locationbias
+                                });
+                            }
                         });
                     }
                 })
             }
         })  
-    });
+    })
 }
 
 //TODO: ASYNC DOESN'T WORK! We need the loop to wait until the download is done to send the processImgOnly call
@@ -51,20 +59,63 @@ function callGoogle() {
 		let latitude = 45.512410;
 		let longitude = -73.569356;
 
+        let locationBiasArray = []
 		for (var i = 0; i < 0.5; i+= 0.05){
 			for(var j = 0; j < 0.5; j+= 0.05){
 				console.log(`i = ${i}`)
-				console.log(`j = ${j}`)
-				downloadLocationPicture("public", `circle:20@${latitude},${longitude}`, "./images/google.jpg")
-				.then( (result) => {
-					index.processImgOnly(latitude, longitude)
-					longitude += j;
-				}) 
-				
+                console.log(`j = ${j}`)
+                locationBiasArray.push(`circle:20@${latitude},${longitude}`);
+                longitude += j;
 			}
 			longitude = -73.569356;
 			latitude += i;
-		}
+        }
+        console.log("==================================")
+        console.log(locationBiasArray.length);
+        console.log("==================================")
+
+        let locationInfo = []
+        for (let index = 0; index < 10; index+=3) {
+            downloadLocationPicture("public", locationBiasArray[index], `./seed-images/${Date.now()}.jpg`)
+            .then((res)=>{
+                console.log(res)
+                locationInfo.push(res);
+                if(locationInfo.length == 3) {
+                    console.log(locationInfo);
+                }
+            })
+        }
+        Promise.map(locationBiasArray, function(locationBias) {
+            return downloadLocationPicture("public", locationBias, `./seed-images/${Date.now()}.jpg`)
+        }, {concurrency:3})
+        .then((info)=>{
+            console.log("ingo")
+            console.log(info);
+        })
+        .catch((err)=>{
+            console.log("errro")
+            console.log(err)
+        });
+}
+
+
+function getAllPromiseData(infoArray, index,  concurrencyMax, data) {
+    
+    for (let i = index; i < index+concurrencyMax && i < infoArray.length; i++) {
+        downloadLocationPicture("public", infoArray[i], `./seed-images/${Date.now()}.jpg`)
+        .then((res)=>{
+            console.log(res)
+            locationInfo.push(res);
+            if(data.length ===infoArray.length){
+                // resolveThepromise with the data
+            }
+            if(data.length === index+concurrencyMax) {
+                setTimeout(function(){
+                    resolve(getAllPromiseData(infoArray, i+1, concurrencyMax, data))
+                }, 500);
+            }
+        })
+    }
 }
 
 callGoogle()
