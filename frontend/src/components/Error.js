a0860c1dcd3ea0ae857e1b1b57c09f2104b2449a
MohamedBeydoun <mohamed.beydoun@mail.mcgill.ca>
2018-12-01 19:15:45 -0500

    styled error page

--- frontend/src/components/Error.js
+++ frontend/src/components/Error.js
@@ -1,9 +1,15 @@
 import React from "react";
+import AccessibleForward from '@material-ui/icons/AccessibleForward'
 
 export default function ErrorPage(props){
     return(
         <div>
-            
+            <div>
+                <AccessibleForward style={{ color: `#3f51b5`, margin: `15% 0 0 46%`, transform: `scale(15)` }} />
+            </div>
+            <div>
+                <p style={{ color: `#3f51b5`, margin: `15% 0 0 30%`, fontSize: `3em` }}> Oops...Page does not exist</p>
+            </div>
         </div>
     )
 }
