6049c31d678de621a4f89ed36533946d9edd4c1c
niiita <nitachaska@gmail.com>
2018-12-02 07:09:18 -0500

    bkgrnd

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -6,7 +6,16 @@ import { withStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 
+// import bkgrnd from "./bkgrnd.png"
 import BottomNavigation from '@material-ui/core/BottomNavigation';
+
+
+// let imgUrl = bkrgnd
+// var sectionStyle = {
+//   width: "100%",
+//   height: "400px",
+//   backgroundImage: 'url(' + imgUrl + ')', 
+// };
 
 const styles = {
     root: {
@@ -20,8 +29,8 @@ const styles = {
     const { classes } = props;
     return (
       <div className={classes.root}>
-        
-        <BottomNavigation>
+        {/* //style={{backgroundImage: 'url(' + imgUrl + ')'}} */}
+        <BottomNavigation >
             <Typography variant="h6" color="inherit">
             <h3><img src={gcpLogo} height="80px" width="80px" alt="missing"/>POWERED BY GCP, INSPIRED BY JP MORGAN <img src={jpmcLogo} height="65px" width="420px" alt="missing"/></h3>
             </Typography></BottomNavigation>
