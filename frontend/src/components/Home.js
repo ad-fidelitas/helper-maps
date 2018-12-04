155f7d12881c9858fb2a58d7da3382c218094913
MohamedBeydoun <mohamed.beydoun@mail.mcgill.ca>
2018-12-03 23:45:11 -0500

    fixed typos

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -144,14 +144,3 @@ HomePage.propTypes = {
 }
 
 export default withStyles(styles)(HomePage);
-
-// export default function HomePage(props) {
-//     // You can compute or insert data for the homepage here if you want
-//     return(
-//         <div>
-//             {/* Put the HTML here, 
-//             if you want to use classes, use the clasnName attribute instead of the class sticker */}
-            
-//         </div>
-//     )
-// }
